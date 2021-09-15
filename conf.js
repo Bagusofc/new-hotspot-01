c1ea489db7709c66561fa736ff12122838576466
Bagusofc <90519223+Bagusofc@users.noreply.github.com>
2021-09-16 05:28:10 +0700

    Update conf.js

--- conf.js
+++ conf.js
@@ -2,7 +2,7 @@ var config = {
 
 loginvc : "Masukkan Kode Voucher kemudian klik Connect.",
 loginup : "Masukkan Username dan Password <br> kemudian klik Connect.",
-voucherCode : "Kode Voucher",
+voucherCode : "BELOR50",
 setCase : "none", // lowercase, uppercase or none
 defaultMode : "voucher", // voucher or member
 theme : "default", // default, dark, lite
